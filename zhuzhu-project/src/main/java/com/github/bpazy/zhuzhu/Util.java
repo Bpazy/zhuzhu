c05b0fe445a44d33b71a666fd9d23d811bf1e5b5
ziyuan <hanziyuan08@outlook.com>
2018-12-29 18:01:03 +0800

    refactor: remove url normalize

--- zhuzhu-project/src/main/java/com/github/bpazy/zhuzhu/Util.java
+++ zhuzhu-project/src/main/java/com/github/bpazy/zhuzhu/Util.java
@@ -1,15 +1,12 @@
 package com.github.bpazy.zhuzhu;
 
-import lombok.SneakyThrows;
 import lombok.experimental.UtilityClass;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.IOUtils;
-import org.apache.commons.lang3.StringUtils;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
 import java.io.IOException;
-import java.net.URL;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -31,31 +28,5 @@ public class Util {
                 .eachAttr("abs:href").stream()
                 .filter(u -> u.startsWith("http"))
                 .collect(Collectors.toList());
-    }
-
-
-    @SneakyThrows
-    public static String getDomain(String fullUrl) {
-        URL url = new URL(fullUrl);
-        return url.getProtocol() + "://" + url.getHost();
-    }
-
-    @SneakyThrows
-    public static String normalizeUrl(String u) {
-        URL url = new URL(u);
-        String s = url.getProtocol() + "://" + url.getHost() + url.getPath();
-        if (StringUtils.isNotBlank(url.getQuery())) {
-            s = s + "?" + url.getQuery()
-                    .replaceAll("\\s", "%20")
-                    .replaceAll("/", "%2F")
-                    .replaceAll("\\+", "%2B")
-                    .replaceAll("\\?", "%3F")
-                    .replaceAll("#", "%23");
-            // TODO Exception in thread "pool-1-thread-1" java.lang.IllegalArgumentException: Illegal character in query at index 59: https://sv.baidu.com/videoui/page/videoland?pd=bjh&context={%22nid%22:%2214291184864727607922%22,%22sourceFrom%22:%22bjh%22}&fr=bjhauthor&type=video
-        }
-        if (StringUtils.isNoneEmpty(url.getRef())) {
-            s = s + "#" + url.getRef();
-        }
-        return s;
     }
 }
--- zhuzhu-project/src/test/java/com/github/bpazy/zhuzhu/UtilTest.java
+++ zhuzhu-project/src/test/java/com/github/bpazy/zhuzhu/UtilTest.java
@@ -1,23 +1,4 @@
 package com.github.bpazy.zhuzhu;
 
-import org.junit.Test;
-
-import static org.junit.Assert.assertEquals;
-
 public class UtilTest {
-
-    @Test
-    public void testDecodeUrl() {
-        assertEquals("http://example.com/login?login=login", Util.normalizeUrl("http://example.com/login?login=login"));
-        assertEquals("http://example.com/login?login=login###", Util.normalizeUrl("http://example.com/login?login=login###"));
-        assertEquals("http://example.com/login?login=login#/123", Util.normalizeUrl("http://example.com/login?login=login#/123"));
-        assertEquals("http://example.com/login?login=login%2B123", Util.normalizeUrl("http://example.com/login?login=login+123"));
-        assertEquals("http://example.com/login?login=login%3F123", Util.normalizeUrl("http://example.com/login?login=login?123"));
-    }
-
-    @Test
-    public void testGetDomain() {
-        assertEquals("http://www.example.com", Util.getDomain("http://www.example.com/abc"));
-        assertEquals("http://www.example.com", Util.getDomain("http://www.example.com?a=a"));
-    }
 }
