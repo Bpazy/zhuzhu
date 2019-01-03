7d3159c434933220469ded197b512602f5ca6364
ziyuan <hanziyuan08@outlook.com>
2019-01-03 21:18:54 +0800

    feat: add 99114 samples, it's a good test for program

---
+++ zhuzhu-samples/zhuzhu-samples-99114/src/main/java/zhuzhu/Zhuzhu99144.java
@@ -0,0 +1,44 @@
+package zhuzhu;
+
+import com.github.bpazy.zhuzhu.CrawlerController;
+import com.github.bpazy.zhuzhu.WebCrawler;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * @author ziyuan
+ */
+public class Zhuzhu99144 {
+
+    public static void main(String[] args) {
+        CrawlerController controller = new CrawlerController();
+        controller.addSeed("http://shop.99114.com/");
+        controller.setThreadNum(5);
+        controller.start(MyWebCrawler.class);
+    }
+
+    @Slf4j
+    public static class MyWebCrawler implements WebCrawler {
+        private static Pattern pattern = Pattern.compile("http://shop\\.99114\\.com/(\\d+)$");
+
+        @Override
+        public boolean shouldVisit(String url) {
+            return url.contains("http://shop.99114.com/");
+        }
+
+        @Override
+        @SneakyThrows
+        public void visit(String url, byte[] content) {
+            Matcher matcher = pattern.matcher(url);
+            if (!matcher.find()) return;
+
+            Document doc = Jsoup.parse(new String(content));
+            log.info("url: {}, title: {}", url, doc.title());
+        }
+    }
+}
