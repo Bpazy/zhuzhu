3dea413bcd8ff65d4218d2f8ff1c8f0f5d3253f8
ziyuan <hanziyuan08@outlook.com>
2019-01-18 12:35:41 +0800

    feat: codecov test

---
+++ zhuzhu-project/src/test/java/com/github/bpazy/zhuzhu/UtilTest.java
@@ -0,0 +1,15 @@
+package com.github.bpazy.zhuzhu;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author ziyuan
+ */
+public class UtilTest {
+
+    @Test
+    public void extractUrls() {
+    }
+}
