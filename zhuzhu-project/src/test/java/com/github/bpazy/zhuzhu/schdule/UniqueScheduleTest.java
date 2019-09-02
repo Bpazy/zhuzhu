0a45df4812890f488d1917d601aa33c43ea63434
ziyuan <hanziyuan08@outlook.com>
2019-09-02 09:37:40 +0800

    feat: add more unit test

--- zhuzhu-project/src/test/java/com/github/bpazy/zhuzhu/schdule/UniqueScheduleTest.java
+++ zhuzhu-project/src/test/java/com/github/bpazy/zhuzhu/schdule/UniqueScheduleTest.java
@@ -26,9 +26,12 @@ public class UniqueScheduleTest {
     public void oneThread() {
         String seed = "https://github.com/Bpazy";
         schedule.add(seed);
+        schedule.add(seed);
         List<String> seeds = Reflect.on(schedule).get("seeds");
         assertThat(seeds).hasSize(1);
         assertThat(schedule.take()).isEqualTo(seed);
+        assertThat(seeds).hasSize(0);
+        assertThat(schedule.take()).isEqualTo("");
         assertThat(seeds).hasSize(0);
     }
 
@@ -46,15 +49,18 @@ public class UniqueScheduleTest {
         }
         latch.await();
 
-        List<String> expectedUrls = Lists.newArrayListWithExpectedSize(100);
+        List<String> expectedUrls = Lists.newArrayListWithExpectedSize(threadNum);
         for (int i = 0; i < threadNum; i++) {
             expectedUrls.add("https://github.com/Bpazy/" + i);
         }
         List<String> seeds = Reflect.on(schedule).get("seeds");
-        assertThat(seeds).hasSize(100);
+        assertThat(seeds).hasSize(threadNum);
+
+        List<String> actualUrls = Lists.newArrayListWithCapacity(threadNum);
         for (int i = 0; i < threadNum; i++) {
-            assertThat(expectedUrls).contains(schedule.take());
+            actualUrls.add(schedule.take());
         }
         assertThat(seeds).hasSize(0);
+        assertThat(actualUrls).containsAll(expectedUrls);
     }
 }
