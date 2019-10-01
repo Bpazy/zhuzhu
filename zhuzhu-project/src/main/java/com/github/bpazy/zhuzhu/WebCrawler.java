c9e9807ac7e13bc43daad2f8de4fbd0c6268a08b
ziyuan <hanziyuan08@outlook.com>
2019-10-01 22:41:11 +0800

    doc: add WebCrawler.visit document

--- zhuzhu-project/src/main/java/com/github/bpazy/zhuzhu/WebCrawler.java
+++ zhuzhu-project/src/main/java/com/github/bpazy/zhuzhu/WebCrawler.java
@@ -6,7 +6,14 @@ package com.github.bpazy.zhuzhu;
 public interface WebCrawler {
     boolean shouldVisit(String url);
 
-    // TODO Maybe object wrapper is needed because of multiple parameters.
+
+    /**
+     * There should be no operations that are blocked for too long because the crawler thread pool is used
+     * TODO Maybe object wrapper is needed because of multiple parameters.
+     *
+     * @param url
+     * @param content
+     */
     void visit(String url, byte[] content);
 
     // TODO Maybe pipeline is better.
