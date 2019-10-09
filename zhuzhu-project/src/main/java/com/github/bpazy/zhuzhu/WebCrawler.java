5517ff36f8211561571f58e61b52c48f66f800f8
ziyuan <hanziyuan08@outlook.com>
2019-10-09 19:42:04 +0800

    style: Remove TODO

--- zhuzhu-project/src/main/java/com/github/bpazy/zhuzhu/WebCrawler.java
+++ zhuzhu-project/src/main/java/com/github/bpazy/zhuzhu/WebCrawler.java
@@ -6,13 +6,8 @@ package com.github.bpazy.zhuzhu;
 public interface WebCrawler<T> {
     boolean shouldVisit(String url);
 
-
     /**
      * There should be no operations that are blocked for too long because the crawler thread pool is used
-     * TODO Maybe object wrapper is needed because of multiple parameters.
-     *
-     * @param url
-     * @param content
      */
     T visit(String url, byte[] content);
 
